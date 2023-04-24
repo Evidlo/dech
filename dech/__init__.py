f9564eb99213c5754efb8316bb4360232979e6bd
evan <evan@evanw.org>
2023-04-24 01:36:45 -0500

    support animation

--- dech/__init__.py
+++ dech/__init__.py
@@ -1,88 +1,9 @@
 from pathlib import Path
-from tempfile import gettempdir
-from io import BytesIO
-# import PIL.Image
-import imageio
-import base64
 import textwrap
 from importlib import resources
 
-class Element:
-    """Parent class for building blocks on a page"""
-
-    def save(self, outfile):
-        raise NotImplementedError
-
-    def html(self, context):
-        """Generate HTML for this element
-
-        Args:
-            context (dict): extra variables used in generating HTML
-
-        Returns:
-            html (str): generated HTML
-        """
-        raise NotImplementedError
-
-
-class Img(Element):
-    """Generate <img> tag for PNGs or GIFs"""
-
-    def __init__(self, content, gif=False, title=None, width=None, height=None):
-        """Initialize Img element
-
-        Args:
-            content (str, matplotlib figure, or ndarray): image content
-
-            FIXME: not implemented
-            gif (bool): whether content is an animation
-
-            title (str): image title field
-            width (int or str): image width in pixels
-        """
-        self.content = content
-        self.title = title
-        self.width = width
-        self.height = height
-
-    def html(self, context):
-
-        # generate styling
-        styles = []
-        width = self.width
-        if type(width) is int:
-            width = f"{width}px"
-        if width is not None:
-            styles.append(f"width:{width}")
-        height = self.height
-        if type(height) is int:
-            height = f"{height}px"
-        if height is not None:
-            styles.append(f"width:{width}")
-
-        # if given path to image
-        if type(self.content) is str:
-            src = self.content
-
-        # if given matplotlib figure
-        if type(self.content).__name__ == 'Figure':
-            buff = BytesIO()
-            self.content.savefig(buff, format='png')
-            src = 'data:image/png;base64,{}'.format(
-                base64.b64encode(buff.getvalue()).decode()
-            )
-
-        # if given numpy array
-        if type(self.content).__name__ == 'ndarray':
-            buff = BytesIO()
-            imageio.imsave(buff, (255 * self.content).astype('uint8'), format='png')
-            src = 'data:image/png;base64,{}'.format(
-                base64.b64encode(buff.getvalue()).decode()
-            )
-            styles.append("image-rendering:crisp-edges")
-
-        style = ";".join(styles)
-        return f'<img style="{style}" src="{src}"/>'
+from .img import Img
+from .element import Element
 
 class Figure(Element):
     """Generate <figure> and <figcaption>"""
@@ -92,11 +13,12 @@ class Figure(Element):
         self.element = element
         self.pos = pos
 
-    def html(self, context):
+    def html(self, context={}):
         el_html = self.element.html(context)
         cap_html = self.caption
         if self.pos == 'bottom':
             return f'<figure class="figure"><figcaption>{cap_html}</figcaption>{el_html}</figure>'
+
 
 class Grid(Element):
     """Elements in a grid with each row separated by a <br>"""
@@ -109,7 +31,7 @@ class Grid(Element):
         """
         self.content = content
 
-    def html(self, context):
+    def html(self, context={}):
         row_strs = []
         if type(self.content) is list:
             for row in self.content:
@@ -129,6 +51,7 @@ class Grid(Element):
             result += f'<div class="row">{row_str}</div>'
 
         return result
+
 
 class Page(Grid):
     """Header + footer wrapper to make complete HTML page"""
@@ -165,7 +88,7 @@ class Page(Grid):
         else:
             self.footer = footer
 
-    def html(self, context):
+    def html(self, context={}):
         return self.header + super().html(context) + self.footer
 
     def save(self, outfile):
--- dech/test_all.py
+++ dech/test_all.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python3
 
 from dech import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 def test_img():
+    # test path string input
     h = Img('/tmp/test.png').html()
-    assert h == '<img src="/tmp/test.png"/>'
+    assert 'src="/tmp/test.png"' in h
+
+    # test numpy array input
+    # grayscale
+    Img(np.random.random((10, 10)), width='1em').html()
+    # color
+    Img(np.random.random((10, 10, 3))).html()
+    # animated grayscale
+    Img(np.random.random((10, 10, 10)), animation=True).html()
+    # animated color
+    Img(np.random.random((10, 10, 10, 3)), animation=True).html()
+    Img(np.random.random((10, 10, 10, 3)), rescale=True, animation=True).html()
+    Img(np.random.random((10, 10, 10, 3)), duration=100, animation=True).html()
+
+    # test matplotlib figure input
+    plt.figure('test')
+    plt.plot(np.random.random(10))
+    Img(plt.figure('test')).html()
+
+
+def test_page(tmp_path):
+    Page([]).save(tmp_path / "test.html")
+    Page([Img('/tmp/test.png')]).save(tmp_path / "test.html")
